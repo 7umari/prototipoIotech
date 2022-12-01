5a5a6d9ccea6f611ea61be60e4d5ec79589d2106
7umari <maria100@estudante.firjan.senai.br>
2022-12-01 15:03:23 -0300

    Tela Principal com os botôes feitos

--- prototipoIotech/telaPrincipal.xaml.cs
+++ prototipoIotech/telaPrincipal.xaml.cs
@@ -23,5 +23,81 @@ namespace prototipoIotech.View
         {
             InitializeComponent();
         }
+
+        private void ProximaTela(object sender, MouseButtonEventArgs e)
+        {
+            objRetalguloLuzesDacasa1.Visibility = Visibility.Hidden;
+            objRetalguloLuzesDacasa2.Visibility = Visibility.Hidden;
+            textLuzesCasa.Visibility = Visibility.Hidden;
+            btnLed1.Visibility = Visibility.Hidden;
+            btnLed2.Visibility = Visibility.Hidden;
+            btnLed3.Visibility = Visibility.Hidden;
+            btnLed4.Visibility = Visibility.Hidden;
+            objRetalguloPorta1.Visibility = Visibility.Hidden;
+            objRetalguloPorta2.Visibility = Visibility.Hidden;
+            btnAbrirPorta.Visibility = Visibility.Hidden;
+
+            btnProximaTela.Visibility = Visibility.Hidden;
+
+            btnVoltarTela.Visibility = Visibility.Visible;
+            objRetalguloLCD1.Visibility = Visibility.Visible;
+            objRetalguloLCD2.Visibility = Visibility.Visible;
+            objRetalguloBuzzer1.Visibility = Visibility.Visible;
+            objRetalguloBuzzer2.Visibility = Visibility.Visible;
+            objRetalguloSensor1.Visibility = Visibility.Visible;
+            objRetalguloSensor2.Visibility = Visibility.Visible;
+            btnBuzzer.Visibility = Visibility.Visible;
+            btnLCD.Visibility = Visibility.Visible;
+            btnSensor.Visibility = Visibility.Visible;
+
+        }
+
+        private void AbrirMenu(object sender, MouseButtonEventArgs e)
+        {
+            objTelaMenu.Visibility = Visibility.Visible;
+            btnConectarCasa.Visibility = Visibility.Visible;
+            btnVoltarMenu.Visibility = Visibility.Visible;
+            btnSairConta.Visibility = Visibility.Visible;
+            btnCriarUsuario.Visibility = Visibility.Visible;
+            imgLogoSemTexto.Visibility = Visibility.Visible;
+        }
+
+        private void VoltarMenu(object sender, MouseButtonEventArgs e)
+        {
+            objTelaMenu.Visibility = Visibility.Hidden;
+            btnConectarCasa.Visibility = Visibility.Hidden;
+            btnVoltarMenu.Visibility = Visibility.Hidden;
+            btnSairConta.Visibility = Visibility.Hidden;
+            btnCriarUsuario.Visibility = Visibility.Hidden;
+            imgLogoSemTexto.Visibility = Visibility.Hidden;
+        }
+
+
+        private void VoltarTela(object sender, MouseButtonEventArgs e)
+        {
+            objRetalguloLuzesDacasa1.Visibility = Visibility.Visible;
+            objRetalguloLuzesDacasa2.Visibility = Visibility.Visible;
+            textLuzesCasa.Visibility = Visibility.Visible;
+            btnLed1.Visibility = Visibility.Visible;
+            btnLed2.Visibility = Visibility.Visible;
+            btnLed3.Visibility = Visibility.Visible;
+            btnLed4.Visibility = Visibility.Visible;
+            objRetalguloPorta1.Visibility = Visibility.Visible;
+            objRetalguloPorta2.Visibility = Visibility.Visible;
+            btnAbrirPorta.Visibility = Visibility.Visible;
+
+            btnProximaTela.Visibility = Visibility.Visible;
+
+            btnVoltarTela.Visibility = Visibility.Hidden;
+            objRetalguloLCD1.Visibility = Visibility.Hidden;
+            objRetalguloLCD2.Visibility = Visibility.Hidden;
+            objRetalguloBuzzer1.Visibility = Visibility.Hidden;
+            objRetalguloBuzzer2.Visibility = Visibility.Hidden;
+            objRetalguloSensor1.Visibility = Visibility.Hidden;
+            objRetalguloSensor2.Visibility = Visibility.Hidden;
+            btnBuzzer.Visibility = Visibility.Hidden;
+            btnLCD.Visibility = Visibility.Hidden;
+            btnSensor.Visibility = Visibility.Hidden;
+        }
     }
 }
